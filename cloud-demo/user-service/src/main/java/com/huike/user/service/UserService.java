2179f42dbf65bc74d3d74a06b8eafb7b3005fb8a
Nie-zhi <m18267975556@163.com>
2026-04-30 18:36:59 +0900

    Add files via upload

---
+++ cloud-demo/user-service/src/main/java/com/huike/user/service/UserService.java
@@ -0,0 +1,17 @@
+package com.huike.user.service;
+
+import com.huike.user.mapper.UserMapper;
+import com.huike.user.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return userMapper.findById(id);
+    }
+}
